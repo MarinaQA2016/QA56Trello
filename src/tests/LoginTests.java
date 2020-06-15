387d824a1bc4af2d29250177fb8e6a705bf4e9c4
MarinaA2015 <m.abrashkina2015@gmail.com>
2020-06-15 11:22:02 +0300

    Tests for LoginTests class (HW 08.06)

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -42,7 +43,7 @@ public class LoginTests {
     }
 
     @Test
-    public void logiNegativeNoLoginNoPassword() throws InterruptedException {
+    public void loginNegativeNoLoginNoPassword() throws InterruptedException {
         //--- Press log In menu button
         driver.findElement(By.linkText("Log In")).click();
         Thread.sleep(5000);
@@ -54,6 +55,54 @@ public class LoginTests {
         System.out.println("Error message: " + driver.findElement(By.cssSelector("#error>p")).getText());
         Thread.sleep(5000);
     }
+
+    @Test
+    public void NegativeLoginIncorrect() throws InterruptedException {
+        //--- Press log In menu button
+        driver.findElement(By.linkText("Log In")).click();
+        Thread.sleep(5000);
+
+        //--- Enter Incorrect Login
+        WebElement loginField = driver.findElement(By.id("user"));
+        loginField.sendKeys("test@test.com");
+        Thread.sleep(7000);
+
+        //----Click 'Log in' button ----
+
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(10000);
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@id = 'error']/p"));
+        System.out.println("Error message: " + errorMessage.getText());
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void NegativePasswordIncorrect() throws InterruptedException {
+        //--- Press log In menu button
+        driver.findElement(By.linkText("Log In")).click();
+        Thread.sleep(5000);
+
+        //--- Enter Correct Login
+        WebElement loginField = driver.findElement(By.id("user"));
+        loginField.sendKeys("marinaqatest2019@gmail.com");
+        Thread.sleep(5000);
+
+        //----Click 'Log in' button ----
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(5000);
+
+        //---Enter incorrect password ---
+        WebElement passwordLogin = driver.findElement(By.id("password"));
+        passwordLogin.sendKeys("error");
+        driver.findElement(By.id("login-submit")).click();
+        Thread.sleep(5000);
+        //---Print error message ---
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@id='login-error']/span"));
+        System.out.println("Error message: " + errorMessage.getText());
+        Thread.sleep(5000);
+    }
+
+
 
     @AfterMethod
     public void tearDown(){
