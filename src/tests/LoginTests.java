d049a262e2244b12f9ee3239ed833c81e8b2b196
MarinaA2015 <m.abrashkina2015@gmail.com>
2020-06-22 11:52:46 +0300

    Classwork, wait-methods

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,45 +14,48 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase{
 
     @Test
-    public void loginTestPositive() throws InterruptedException {
+    public void loginTestPositive()  {
 
         //--- Press log In menu button
         driver.findElement(By.linkText("Log In")).click();
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.id("login"),10);
 
         //----Enter login value and click 'Log in' button ----
         driver.findElement(By.id("user")).sendKeys(LOGIN);
-        Thread.sleep(2000);
+        waitUntilAttributeValueIs(By.
+                id("login"),"value","Log in with Atlassian",10);
         driver.findElement(By.id("login")).click();
-        Thread.sleep(10000);
+
+        waitUntilElementIsClickable(By.id("login-submit"),15);
 
         //---- Enter password value and click 'Log in' button
         driver.findElement(By.id("password")).sendKeys(PASSWORD);
         driver.findElement(By.id("login-submit")).click();
-        Thread.sleep(35000);
+
+        waitUntilElementIsClickable(By
+                .xpath("//button[@data-test-id='header-boards-menu-button']/span[2]"),40);
         WebElement boardIcon = driver.findElement(By
                 .xpath("//button[@data-test-id='header-boards-menu-button']/span[2]"));
-        System.out.println("'Boards' button text: " + boardIcon.getText());
-        Thread.sleep(5000);
+
         Assert.assertEquals(boardIcon.getText(),"Boards","Text on the boardIcon is not 'Boards'");
 
     }
 
+
     @Test
-    public void loginNegativeNoLoginNoPassword() throws InterruptedException {
+    public void loginNegativeNoLoginNoPassword()  {
         //--- Press log In menu button
         driver.findElement(By.linkText("Log In")).click();
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.id("login"),10);
 
         //----Click 'Log in' button ----
-
         driver.findElement(By.id("login")).click();
-        Thread.sleep(5000);
+        waitUntilElementIsVisible(By.cssSelector("#error>p"),10);
         WebElement errorMessage = driver.findElement(By.cssSelector("#error>p"));
         System.out.println("Error message: " + errorMessage.getText());
-        Thread.sleep(5000);
         Assert.assertTrue(errorMessage.getText().equals("Missing email"));
     }
+
 
     @Test
     public void NegativeLoginIncorrect() throws InterruptedException {
@@ -100,8 +105,6 @@ public class LoginTests extends TestBase{
         Assert.assertTrue(errorMessage.getText().contains("Incorrect email address and / or password."),
                 "There is no error message or the text of the message is not correct");
     }
-
-
 
 
 }
--- src/tests/TestBase.java
+++ src/tests/TestBase.java
@@ -1,7 +1,10 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -17,11 +20,39 @@ public class TestBase {
         //---- Enter to the application ---
         driver = new ChromeDriver();
         driver.get("https://trello.com/");
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.linkText("Log In"),20);
+
     }
 
     @AfterMethod
     public void tearDown(){
         driver.quit();
     }
+
+    public void waitUntilElementIsClickable(By locator, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions
+                    .elementToBeClickable(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitUntilElementIsVisible(By locator, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions
+                    .visibilityOfElementLocated(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void waitUntilAttributeValueIs(By locator, String attribute, String value, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions
+                    .attributeToBe(locator,attribute,value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
